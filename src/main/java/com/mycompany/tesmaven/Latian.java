3cd08cb7a2c1e0d4afee9f51ea6a3243b46d8646
A-21 <A-21@A-21>
2024-04-01 10:41:48 +0700

    ini upload dari netbeans

---
+++ src/main/java/com/mycompany/tesmaven/Latian.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tesmaven;
+
+/**
+ *
+ * @author A-21
+ */
+public class Latian {
+    public static void main(String[] args) {
+        System.out.println("hello world!!, lorem ipsum dolor sit amet");
+    }
+}
